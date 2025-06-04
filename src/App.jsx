a2efc89c2751515b642262ae2d8827206ed46621
TorresAnndy <andygonzales2003@gmail.com>
2025-06-04 00:51:09 -0500

    new file:   .gitignore 	new file:   README.md 	new file:   eslint.config.js 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.jsx 	new file:   src/assets/react.svg 	new file:   src/css/App.css 	new file:   src/css/Menu.css 	new file:   src/css/index.css 	new file:   src/logos/github.png 	new file:   src/logos/image.png 	new file:   src/main.jsx 	new file:   src/pages/Ejercicio1.jsx 	new file:   src/pages/Home.jsx 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+import './css/Menu.css'
+import './css/App.css'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Ejercicio1 from './pages/Ejercicio1.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+      <BrowserRouter>
+          <nav>
+            <ul>
+              <li><Link to='/'>Inicio</Link></li>
+              <li><Link to='/ejercicio1'>Ejercicio1</Link></li>
+              
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/ejercicio1' element={<Ejercicio1 />} />
+          </Routes>
+        </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Ejercicio1.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Ejercicio1 extends Component {
+  render() {
+    return (
+      <div>Ejercicio1</div>
+    )
+  }
+}
+
+export default Ejercicio1
